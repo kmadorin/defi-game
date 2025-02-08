546a5478c1e5e99017858f0a98a7d454aacf4983
Kirill <madorinkv@gmail.com>
2025-02-08 21:04:55 +0100

    vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
- 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
+
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['crypto']
+    })
+  ],
+  define: {
+    // Fallback for crypto.randomUUID
+    'crypto.randomUUID': 'crypto.randomUUID || (() => Math.random().toString(36).substring(2))'
+  }
 })
